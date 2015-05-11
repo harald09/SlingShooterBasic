3d1fbffd19092162a9ed403a87a1812f4fb75ca2
Harald <konstantin.schorer@stud.h-da.de>
2015-05-11 15:56:40 +0200

    SlingShot->HALO

---
+++ Assets/Scripts/Slingshot.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Slingshot : MonoBehaviour {
+
+	public GameObject launchPoint;
+
+	void Awake(){
+		Transform launchPointTransform = transform.Find("LaunchPoint");
+		launchPoint = launchPointTransform.gameObject;
+		launchPoint.SetActive(false);
+
+	}
+
+	void OnMouseEnter(){
+		launchPoint.SetActive(true);
+		//print ("hey");
+	}
+	void OnMouseExit(){
+		launchPoint.SetActive(false);
+	}
+}
